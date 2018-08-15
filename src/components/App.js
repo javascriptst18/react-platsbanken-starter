38383efd65c5298be1c104e5db7b2936a6e9c45f
Jesper Orb <jesper.orb@zocom.se>
2018-08-15 10:46:23 +0200

    React router basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Annonser from './Annonser';
 import '../styles/App.css';
 
-class App extends Component {
-  render() {
-    return <Annonser />; 
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/"> Home</Link>
+          <Link to="/about"> About </Link>
+          <Link to="/annonser"> Annonser </Link>
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/annonser" component={Annonser} />
+        <Route path="/:namn" component={About} />
+        <Route component={Home} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+function About(props) {
+  console.log(props.location);
+  return <h1> About! </h1>;
+}
+
+function Home(){
+  return <h1>Tjena från home</h1>
 }
 
 export default App;
